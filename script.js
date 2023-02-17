0dcfd95c292d38c02b3c824de3d3a7c071c645c4
foma0g <116730747+foma0g@users.noreply.github.com>
2023-02-17 15:00:33 +0000

    minmax

--- script.js
+++ script.js
@@ -31,88 +31,103 @@ const checkWin = () => {
 	return null;
 };
 
-const minimax = (depth, alpha, beta, maximizingPlayer) => {
-	const result = checkWin();
-	if (result !== null) {
-		if (result === 'X') {
-			return { score: -10 };
-		} else if (result === 'O') {
-			return { score: 10 };
-		} else {
-			return { score: 0 };
-		}
-	}
+function minimax(depth, maximizingPlayer, alpha, beta) {
+    const score = evaluate();
+    if (score === 10) {
+        return score - depth;
+    } else if (score === -10) {
+        return score + depth;
+    } else if (board.includes('') === false) {
+        return 0;
+    }
 
-	if (maximizingPlayer) {
-		let bestScore = -Infinity;
-		let bestMove = null;
-		for (let i = 0; i < board.length; i++) {
-			if (board[i] === '') {
-				board[i] = 'O';
-				const score = minimax(depth + 1, alpha, beta, false).score;
-				board[i] = '';
-				if (score > bestScore) {
-					bestScore = score;
-					bestMove = i;
-				}
-				alpha = Math.max(alpha, bestScore);
-				if (beta <= alpha) {
-					break;
-				}
-			}
-		}
-		return depth === 0 ? bestMove : { score: bestScore };
-	} else {
-		let bestScore = Infinity;
-		let bestMove = null;
-		for (let i = 0; i < board.length; i++) {
-			if (board[i] === '') {
-				board[i] = 'X';
-				const score = minimax(depth + 1, alpha, beta, true).score;
-				board[i] = '';
-				if (score < bestScore) {
-					bestScore = score;
-					bestMove = i;
-				}
-				beta = Math.min(beta, bestScore);
-				if (beta <= alpha) {
-					break;
-				}
-			}
-		}
-		return depth === 0 ? bestMove : { score: bestScore };
-	}
-};
+    if (maximizingPlayer) {
+        let maxScore = -Infinity;
+        for (let i = 0; i < board.length; i++) {
+            if (board[i] === '') {
+                board[i] = bot;
+                let score = minimax(depth + 1, false, alpha, beta);
+                board[i] = '';
+                maxScore = Math.max(maxScore, score);
+                alpha = Math.max(alpha, score);
+                if (beta <= alpha) {
+                    break;
+                }
+            }
+        }
+        return maxScore;
+    } else {
+        let minScore = Infinity;
+        for (let i = 0; i < board.length; i++) {
+            if (board[i] === '') {
+                board[i] = player;
+                let score = minimax(depth + 1, true, alpha, beta);
+                board[i] = '';
+                minScore = Math.min(minScore, score);
+                beta = Math.min(beta, score);
+                if (beta <= alpha) {
+                    break;
+                }
+            }
+        }
+        return minScore;
+    }
+}
 
-const makeMove = (cell, index) => {
-	board[index] = currentPlayer;
-	cell.textContent = currentPlayer;
-	const winner = checkWin();
-	if (winner !== null) {
-		gameOver = true;
-		if (winner === 'X') {
-			message.textContent = 'You win!';
-		} else {
-			message.textContent = 'You lose!';
-		}
-		return;
-	}
-	switchPlayer();
-	const bestMove = minimax(0, -Infinity, Infinity, true);
-	board[bestMove] = currentPlayer;
-	cells[bestMove].textContent = currentPlayer;
-	const computerWinner = checkWin();
-	if (computerWinner !== null) {
-		gameOver = true;
-		if (computerWinner === 'X') {
-			message.textContent = 'You win!';
-		} else {
-			message.textContent = 'You lose!';
-		}
-		return;
-	}
-	switchPlayer();
-};
+function evaluate() {
+    const lines = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+    ];
+
+    for (let i = 0; i < lines.length; i++) {
+        const [a, b, c] = lines[i];
+        if (board[a] !== '' && board[a] === board[b] && board[a] === board[c]) {
+            if (board[a] === bot) {
+                return 10;
+            } else {
+                return -10;
+            }
+        }
+    }
+    return 0;
+}
+
+function makeMove(cell, index) {
+    board[index] = currentPlayer;
+    cell.textContent = currentPlayer;
+    const winner = checkWin();
+    if (winner !== null) {
+        gameOver = true;
+        if (winner === 'X') {
+            message.textContent = 'You win!';
+        } else {
+            message.textContent = 'You lose!';
+        }
+        return;
+    }
+    switchPlayer();
+    const bestMove = minimax(0, -Infinity, Infinity, true);
+    board[bestMove] = currentPlayer;
+    cells[bestMove].textContent = currentPlayer;
+    const computerWinner = checkWin();
+    if (computerWinner !== null) {
+        gameOver = true;
+        if (computerWinner === 'X') {
+            message.textContent = 'You win!';
+        } else {
+            message.textContent = 'You lose!';
+        }
+        return;
+    }
+    switchPlayer();
+}
 
 cells.forEach((cell, index) => {
 	cell.addEventListener('click', () => {
